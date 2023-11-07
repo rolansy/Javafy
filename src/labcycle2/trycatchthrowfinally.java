42ec1c9d2db1f8846967c5e746952bb99c503cca
Ronal <ronalshoeygeorge@gmail.com>
2023-11-07 13:09:33 +0530

    Corrections for Try catch throw

--- src/labcycle2/trycatchthrowfinally.java
+++ src/labcycle2/trycatchthrowfinally.java
@@ -1,17 +1,22 @@
 package labcycle2;
+import java.util.Scanner;
 public class trycatchthrowfinally {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws ArithmeticException {
 		// TODO Auto-generated method stub
 		try{
-				int x=10;
-				int y=0;
+				Scanner sc=new Scanner(System.in);
+				System.out.println("Enter the value of x : ");
+				int x=sc.nextInt();
+				System.out.println("Enter the value of y : ");
+				int y=sc.nextInt();
 				int z=x/y;
+				System.out.println("The result is : "+z);
 	}
 		catch (ArithmeticException e) {
 			System.out.println("Cannot be divided by 0");
 		}finally {
-			System.out.println("Finally Eecuted");
+			System.out.println("Finally Executed");
 			
 		}
 	}
