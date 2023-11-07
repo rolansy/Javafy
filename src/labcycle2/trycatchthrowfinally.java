681f7a606c923ded3e8e509416c3e0c074212706
Ronal <ronalshoeygeorge@gmail.com>
2023-11-07 13:14:01 +0530

    fixes from lab

--- src/labcycle2/trycatchthrowfinally.java
+++ src/labcycle2/trycatchthrowfinally.java
@@ -1,17 +1,19 @@
 package labcycle2;
 import java.util.Scanner;
+
 public class trycatchthrowfinally {
 
 	public static void main(String[] args) throws ArithmeticException {
 		// TODO Auto-generated method stub
 		try{
 				Scanner sc=new Scanner(System.in);
-				System.out.println("Enter the value of x : ");
+				System.out.print("Enter the value of x : ");
 				int x=sc.nextInt();
-				System.out.println("Enter the value of y : ");
+				System.out.print("Enter the value of y : ");
 				int y=sc.nextInt();
 				int z=x/y;
 				System.out.println("The result is : "+z);
+				sc.close();
 	}
 		catch (ArithmeticException e) {
 			System.out.println("Cannot be divided by 0");
