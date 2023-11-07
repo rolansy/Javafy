8ba8950a14a14206dff23d7744b26eb61c19db1f
Ronal <ronalshoeygeorge@gmail.com>
2023-11-07 13:58:12 +0530

    Filehandled

--- src/labcycle2/FileHandling.java
+++ src/labcycle2/FileHandling.java
@@ -1,26 +1,27 @@
 package labcycle2;
-
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileInputStream;
-
-
+import java.io.*;
 public class FileHandling {
-    public static void main (String[] args ) throws IOException{    	
-    try {
-    	FileInputStream f1 = new FileInputStream("trial.txt");
-    	FileOutputStream f2 = new FileOutputStream("Copy.txt");
-    	int c=0;
-    	do {
-    	c=f1.read();
-    	if (c!=-1) {
-    		f2.write((char)c);
-    		}
-    	}while(c!=-1);
-    }
-    catch (FileNotFoundException e) {
-    	System.out.print("File Not Found");
-    	return;
-    }
+    public static void main(String[] args) throws IOException {
+        FileInputStream f1=null;
+        FileOutputStream f2=null;
+        try{
+            f1=new FileInputStream("j.txt");
+            f2= new FileOutputStream("Copy.txt");
+            int c;
+            do{
+                c=f1.read();
+                if(c!=-1){
+                    f2.write((char)c);
+                }
+            }while (c!=-1);
+        }
+        catch(FileNotFoundException e){
+            System.out.println("File not found");
+            return;
+        }
+        finally{
+        	System.out.print("FileHandling Executed");
+        }
+        
     }
 }
