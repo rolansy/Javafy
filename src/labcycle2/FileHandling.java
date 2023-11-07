7dce19dfa09ffadc885d9cb9efbf0301a4fbd235
Ronal <ronalshoeygeorge@gmail.com>
2023-11-07 13:41:20 +0530

    try done

--- src/labcycle2/FileHandling.java
+++ src/labcycle2/FileHandling.java
@@ -1,18 +1,21 @@
 package labcycle2;
 
+import java.io.IOException;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+
+
 public class FileHandling {
-    public static void main (String[] args ) throws IOException{
-    	FileInputStream f1 = null;
-    	FileOututStream f2 = null;
-    	
-    }
+    public static void main (String[] args ) throws IOException{    	
     try {
-    	f1=new FileInputStream("trial.txt")
-    	f2=new FileOutputStream("Copy.txt")
-    	c=0
+    	FileInputStream f1 = new FileInputStream("trial.txt");
+    	FileOutputStream f2 = new FileOutputStream("Copy.txt");
+    	int c=0;
     	do {
-    		
-    		
+    	c=f1.read();
+    	if (c!=-1) {
+    		f2.write((char)c);
+    	}
     	}while(c!=-1);
     }
 }
