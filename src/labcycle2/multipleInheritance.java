5118b9952545e6d58921d53cbb5ec27c99a2cfc3
Ronal <ronalshoeygeorge@gmail.com>
2023-11-07 14:13:57 +0530

    MultipleInheritane student class

---
+++ src/labcycle2/multipleInheritance.java
@@ -0,0 +1,21 @@
+package labcycle2;
+import java.util.*;
+interface Sports{
+	final double sportwt=6.0;
+	abstract void putwt();
+}
+
+class student{
+	int rollno;
+	void getno() {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter roll no : ");
+		rollno=sc.nextInt();
+	}
+	
+}
+
+public class multipleInheritance {
+	public static void main()
+{}
+}
