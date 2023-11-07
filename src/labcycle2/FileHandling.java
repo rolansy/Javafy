386b77eac1a6c217206e6943859067a44b800fb0
Ronal <ronalshoeygeorge@gmail.com>
2023-11-07 13:36:04 +0530

    File handling try

---
+++ src/labcycle2/FileHandling.java
@@ -0,0 +1,18 @@
+package labcycle2;
+
+public class FileHandling {
+    public static void main (String[] args ) throws IOException{
+    	FileInputStream f1 = null;
+    	FileOututStream f2 = null;
+    	
+    }
+    try {
+    	f1=new FileInputStream("trial.txt")
+    	f2=new FileOutputStream("Copy.txt")
+    	c=0
+    	do {
+    		
+    		
+    	}while(c!=-1);
+    }
+}
