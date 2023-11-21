b71f680cea4caeb5a43aec0181d2f3c30f3ac02e
Ronal <ronalshoeygeorge@gmail.com>
2023-11-21 14:57:03 +0530

    Add DLL class and Node class to DoublyLinkedList

--- src/labcycle3/DoublyLinkedList.java
+++ src/labcycle3/DoublyLinkedList.java
@@ -1,5 +1,19 @@
 package labcycle3;
 
+class DLL{
+	class Node{
+		int data;
+		Node llink;
+		Node rlink;
+		public Node(int data) {
+			this.data=data;
+			this.rlink=null;
+			this.llink=null;
+		}
+	}
+	
+}
+
 public class DoublyLinkedList {
 
 	public static void main(String[] args) {
