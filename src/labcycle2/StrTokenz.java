46e92d4eb989ca303cf50c50f3a214daced4c207
Ronal <ronalshoeygeorge@gmail.com>
2023-11-28 13:27:56 +0530

    Add StrTokenz class for summing integers

---
+++ src/labcycle2/StrTokenz.java
@@ -0,0 +1,23 @@
+package labcycle2;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class StrTokenz{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter a line of Integers : ");
+		String s=sc.nextLine();
+		StringTokenizer st=new StringTokenizer(s," ");
+		int sum=0;
+		while(st.hasMoreTokens()) {
+			String str=st.nextToken();
+			int n=Integer.parseInt(str);
+			sum+=n;
+			
+		}
+		System.out.print("Sum : "+sum);
+		sc.close();
+		
+	}
+}
