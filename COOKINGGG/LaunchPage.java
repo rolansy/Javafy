2c6a14d2adbd424f7179818df6c536e7e75472a9
rolansy <ronalshoeygeorge@gmail.com>
2024-02-13 01:40:30 +0530

    Add LaunchPage class with button functionality

---
+++ COOKINGGG/LaunchPage.java
@@ -0,0 +1,30 @@
+package Swingg;
+
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public class LaunchPage implements ActionListener {
+	JFrame frame = new JFrame();
+	JButton button = new JButton("New Window");
+		
+	LaunchPage(){
+		
+		button.setBounds(100,160,200,40);
+		button.addActionListener(this);
+		
+		frame.add(button);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(420,420);
+		frame.setLayout(null);
+		frame.setVisible(true);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==button) {
+			frame.dispose();
+			NewWindow myWindow  = new NewWindow();
+		}
+	}
+}
